b9f3baf06a6c40d7e850553e589a79d9503160c3
dvdrtrgn <dvdrtrgn@gmail.com>
2022-05-19 21:54:33 -0500

    Ready for prod

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const out = 'vuebox';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: { outDir: `dist/vuebox` },
+    build: { outDir: `dist/${out}` },
+    base: `/${out}/`,
     plugins: [vue()],
     resolve: {
         alias: {
